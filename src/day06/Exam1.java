c99d0b43ddc616f00661419760f7c0614c5d07fa
jisung-louis <khkh9357@naver.com>
2026-01-28 15:08:55 +0900

    0128

---
+++ src/day06/Exam1.java
@@ -0,0 +1,11 @@
+package day06;
+
+public class Exam1 {
+
+    // 속성은 객체마다 각각 메모리 할당 (n개)
+    // 메소드는 객체들이 공유 메모리 할당 (1개)
+
+    // student.name, student.lunchbox
+
+    // 예 : 객체가 2개라면 -> 속성 메모리 4개, 메소드 메모리 1개
+}
